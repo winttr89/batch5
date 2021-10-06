82e75b51d5883cc95d28e192d7875ee8bbede7cf
winttr89 <winttr89@gmail.com>
2021-10-06 01:43:07 +0000

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sub="2dc20343-d57b-4b52-ad07-89263986449f"
+sub="1b1aa4c1-4bcb-432f-8957-8384847cf57"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch5/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch5/main/batch2.json
