662707ea343b53df7fc0791b91a6ce9645cba454
winttr89 <winttr89@gmail.com>
2021-10-18 06:14:55 -0700

    1

--- auto1.sh
+++ auto1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sub="97c5a6d9-fa03-43bf-9e96-8941f269a172"
+sub="fb59ec71-ec1b-4273-93a0-534b83d8a0cd"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch5/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch5/main/batch2.json
--- auto3.sh
+++ auto3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sub="75ec3f6e-8464-47e6-a2f0-cbf27557f884"
+sub="ff35d6bc-445f-490f-9f4a-39e8b8d97b7e"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch5/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch5/main/batch2.json
